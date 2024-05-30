69c6b32dbfe1202c0e3cd6099bd36310ad4424ee
Cyberfire <buayagoreng99@gmail.com>
2024-05-30 11:35:08 +0800

    membuat konstruktor untuk inisialisasi queues

--- CircularQueuesArray_0154.cpp
+++ CircularQueuesArray_0154.cpp
@@ -4,7 +4,17 @@ using namespace std;
 class Queues {
     int FRONT, REAR, max = 5;
     int queues_array[5];
+
+
+public:
+    // Constructor untuk inisialisasi queue
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
 };
+
+
 
 int main()
 {
